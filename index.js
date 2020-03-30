c0300e307ae2e84a11515dc38790fadc7ad01714
zuhdi <zudhy.duddy@gmail.com>
2020-03-30 17:00:01 +0700

    done broken palindrome swapper

--- index.js
+++ index.js
@@ -6,20 +6,26 @@
  * @returns {boolean} true bila kata yang di-swap adalah palindrom
  */
 function palindromeSwapper(str) {
-  let newStr = '';
+  if (str.split('').reverse().join('') === str) return true
+
   for (var i = 0; i < str.length; i++) {
-    for (var j = 0; j < str.length; i++) {
+    let newStr = '';
+    for (var j = 0; j < str.length; j++) {
       if (i === j) {
-        newStr += str[j++];
+        newStr += str[j+1];
         newStr += str[j];
         j++;
       } else {
         newStr += str[j];
       }
+      // debugger
     }
     if (isPalindrome(newStr)) return true;
+    // console.log(newStr);
+    // debugger
   }
   return false;
+  
 }
 
 /**
@@ -29,8 +35,9 @@ function palindromeSwapper(str) {
  * @returns {boolean} true bila kata adalah palindrom
  */
 function isPalindrome(str) {
-  if (str.split('').reverse().join() === str) return console.log(true);
-  return console.log(false);
+  if (str.split('').reverse().join('') === str) return true
+  // debugger
+  return false
 }
 
 console.log(palindromeSwapper('arcecar')); // TRUE
