b40deaad7bf627b6851731b3c599c88d21766af1
Mercen1 <nurhabib102@gmail.com>
2020-03-30 19:29:26 +0700

    edit index.js solve

--- index.js
+++ index.js
@@ -7,19 +7,21 @@
  */
 function palindromeSwapper(str) {
   let newStr = '';
+  if (isPalindrome(str)) return 'TRUE';
   for (var i = 0; i < str.length; i++) {
-    for (var j = 0; j < str.length; i++) {
+    for (var j = 0; j < str.length; j++) {
       if (i === j) {
-        newStr += str[j++];
-        newStr += str[j];
+        newStr += str[j+1];
+        newStr += str[j];                                                                                                                                              
         j++;
       } else {
         newStr += str[j];
       }
     }
-    if (isPalindrome(newStr)) return true;
+    if (isPalindrome(newStr)) return 'TRUE';
+    newStr=''
   }
-  return false;
+  return 'FALSE';
 }
 
 /**
@@ -29,10 +31,14 @@ function palindromeSwapper(str) {
  * @returns {boolean} true bila kata adalah palindrom
  */
 function isPalindrome(str) {
-  if (str.split('').reverse().join() === str) return console.log(true);
-  return console.log(false);
+  if (str.split('').reverse().join('') === str) return true;
+  return false;
 }
 
 console.log(palindromeSwapper('arcecar')); // TRUE
 console.log(palindromeSwapper('racecar')); // TRUE
 console.log(palindromeSwapper('recacar')); // FALSE
+
+console.log(palindromeSwapper('ajjaajaja')); // TRUE
+console.log(palindromeSwapper('akumuka')); // TRUE
+console.log(palindromeSwapper('makan')); // FALSE
